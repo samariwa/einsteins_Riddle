695b2ceaff1f6a10f005038278cffe3a851ab993
Mariwa <samuelmariwa@gmail.com>
2021-05-31 21:57:44 +0300

    riddle changes

--- Program.cs
+++ Program.cs
@@ -27,14 +27,48 @@ namespace einsteins_Riddle
                 int points = 0;
                 if (Person.Nationality != null)
                 {
-                    if ((House.Color == "Red") && (Person.Nationality == "Brit"))
+                    if ((Person.Nationality == "Brit") && (House.Color == "Red"))
+                    {
+                        points += 1;
+                    }
+                    else if ((Person.Nationality == "Swede") && (Person.Pet == "Dogs"))
+                    {
+                        points += 1;
+                    }
+                    else if ((Person.Nationality == "Dane") && (Person.Drink == "Tea"))
+                    {
+                        points += 1;
+                    }
+                    else if ((Person.Nationality == "Norwegian") && (House.Number == 1) && (HouseRight.Color == "Blue"))
+                    {
+                        points += 1;
+                    }
+                    else if((Person.Nationality == "German") && (Person.Cigarette == "Prince"))
                     {
                         points += 1;
                     }
                 }
-                else if((Person.Nationality == "Swede") && (Person.Pet == "Dogs"))
+                else if (House.Color != null)
                 {
-                    points += 1;
+                    if ((House.Color == "Green") && (HouseLeft.Color == "White") && (Person.Drink == "Coffee"))
+                    {
+                        points += 1;
+                    }
+                    else if ((House.Color == "Yellow") && (Person.Cigarette == "Dunhill"))
+                    {
+                        points += 1;
+                    }
+                }
+                else if (Person.Cigarette != null)
+                {
+                    if((Person.Cigarette == "Pall Mall") && (Person.Pet == "Birds"))
+                    {
+                        points += 1;
+                    }
+                   /* else if ((Person.Cigarette == "Blend") )
+                    {
+
+                    }*/
                 }
                 return points;
             }
