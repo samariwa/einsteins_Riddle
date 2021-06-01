23e7d91099c978337c75b188a9f7fa56df87a9c3
Mariwa <samuelmariwa@gmail.com>
2021-06-01 21:39:28 +0300

    Riddle Complete

--- Program.cs
+++ Program.cs
@@ -28,17 +28,10 @@ namespace einsteins_Riddle
                 string _pet = Console.ReadLine();
                 Person Obj_Person = new Person(_color, _nationality, _drink, _cigarette, _pet);
                 House Obj_House = new House(_number);
-                /*Console.Write(Obj_House.Number);
-                Console.Write("\n"+Obj_Person.Color);
-                Console.Write("\n" + Obj_Person.Nationality);
-                Console.Write("\n" + Obj_Person.Drink);
-                Console.Write("\n" + Obj_Person.Cigarette);
-                Console.Write("\n" + Obj_Person.Pet);*/
                 object[] values = new object[6] { Obj_House.Number, Obj_Person.Color, Obj_Person.Nationality, Obj_Person.Drink, Obj_Person.Cigarette, Obj_Person.Pet };
                 grid[counter] = values;
                 int Verify()
                 {
-
                     if (Obj_Person.Nationality != null)
                     {
                         if ((Obj_Person.Nationality == "Brit") && (Obj_Person.Color == "Red"))
@@ -53,7 +46,7 @@ namespace einsteins_Riddle
                         {
                             points += 1;
                         }
-                        else if ((Obj_Person.Nationality == "Norwegian" && Obj_House.Number == 1))
+                        else if ((Obj_Person.Nationality == "Norwegian") && (Obj_House.Number == 1))
                         {
                             points += 1;
                         }
@@ -62,83 +55,76 @@ namespace einsteins_Riddle
                             points += 1;
                         }
                     }
-                    else if (Obj_Person.Color != null)
+                     if (Obj_Person.Color != null)
                     {
-                        if (Obj_Person.Color == "White" && grid[counter - 1][1].ToString() == "Green")
+                        if ((Obj_Person.Color == "White") && (counter > 0) && (grid[counter - 1][1].ToString() == "Green"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Color == "Green" && Obj_Person.Drink == "Coffee")
+                        else if ((Obj_Person.Color == "Green") && (Obj_Person.Drink == "Coffee"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Color == "Yellow" && Obj_Person.Cigarette == "Dunhill")
+                        else if ((Obj_Person.Color == "Yellow") && (Obj_Person.Cigarette == "Dunhill"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Color == "Blue" && grid[counter - 1][0].ToString() == "Norwegian")
+                        else if ((Obj_Person.Color == "Blue") && (counter > 0) && (grid[counter - 1][2].ToString() == "Norwegian"))
                         {
                             points += 1;
                         }
                     }
-                    else if (Obj_Person.Cigarette != null)
+                    if (Obj_Person.Cigarette != null)
                     {
-                        if (Obj_Person.Cigarette == "Pall Mall" && Obj_Person.Pet == "Birds")
+                        if ((Obj_Person.Cigarette == "Pall Mall") && (Obj_Person.Pet == "Birds"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Cigarette == "Blue Master" && Obj_Person.Drink == "Beer")
+                        else if ((Obj_Person.Cigarette == "Blue Master") && (Obj_Person.Drink == "Beer"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Cigarette == "Blend" && grid[counter - 1][5].ToString() == "Cats")
+                        else if ((Obj_Person.Cigarette == "Blend") && (grid[counter - 1][5].ToString() == "Cats"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Cigarette == "Blend" && grid[counter - 1][3].ToString() == "Water")
+                        else if ((Obj_Person.Cigarette == "Blend") && (counter > 0) && (grid[counter - 1][3].ToString() == "Water"))
                         {
                             points += 1;
                         }
-                        else if (Obj_Person.Cigarette == "Dunhill" && grid[counter - 1][5].ToString() == "Horses")
+                        else if ((Obj_Person.Cigarette == "Dunhill") && (counter > 0) && (grid[counter - 1][5].ToString() == "Horses"))
+                        {
+                            points += 1;
+                        }
+                        else if ((Obj_Person.Cigarette == "Blends") && (counter > 0) && (grid[counter - 1][3].ToString() == "Water"))
                         {
                             points += 1;
                         }
                     }
-                    else if (Obj_Person.Pet != null)
+                    if (Obj_Person.Pet != null)
                     {
-                        if (Obj_Person.Pet == "Cats" && grid[counter - 1][3].ToString() == "Blend")
-                        {
-                            points += 1;
-                        }
-                        else if (Obj_Person.Pet == "Horses" && grid[counter - 1][3].ToString() == "Dunhill")
+                         if ((Obj_Person.Pet == "Horses") && (counter > 0) && (grid[counter - 1][4].ToString() == "Dunhill"))
                         {
                             points += 1;
                         }
                     }
-                    else if (Obj_Person.Drink != null)
+                    if (Obj_Person.Drink != null)
                     {
-                        if(Obj_Person.Drink == "Water" && grid[counter - 1][3].ToString() == "Blend")
-                        {
-                            points += 1;
-                        }
-                      else if (Obj_Person.Drink == "Milk" &&  Obj_House.Number == 3)
+                       if ((Obj_Person.Drink == "Milk") &&  (Obj_House.Number == 3))
                         {
                             points += 1;
                         }
                     }
-                   
-
                         return points;
                  }
                    result = Verify();
                    counter++;
                  }
-                 
                  if (result == 0)
                  {
                      print = "Failed";
                  }
-                 else if(result == 15)
+                 else if(result == 14)
                  {
                      print = "Passed";
                  }
@@ -146,7 +132,7 @@ namespace einsteins_Riddle
                  {
                      print = "Fair";
                  }
-            Console.WriteLine("Score: "+result);
+            Console.WriteLine("Points scored: "+result);
             Console.WriteLine("Result: "+print);
         }
     }
