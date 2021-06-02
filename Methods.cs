2b5a1b46fac6addc365eeea463e6c788b1f997f3
Mariwa <samuelmariwa@gmail.com>
2021-06-02 07:43:27 +0300

    Complete

---
+++ Methods.cs
@@ -0,0 +1,224 @@
+﻿using System;
+namespace einsteins_Riddle
+{
+     class Methods
+    {
+        public static int Points(int points, int counter, Object [][] grid, int number, string color, string nationality, string drink, string cigar, string pet)
+        {
+            if (nationality != null)
+            {
+                if ((nationality == "Brit") && (color == "Red"))
+                {
+                    points += 1;
+                }
+                else if ((nationality == "Swede") && (pet == "Dogs"))
+                {
+                    points += 1;
+                }
+                else if ((nationality == "Dane") && (drink == "Tea"))
+                {
+                    points += 1;
+                }
+                else if ((nationality == "Norwegian") && (number == 1))
+                {
+                    points += 1;
+                }
+                else if ((nationality == "German") && (cigar == "Prince"))
+                {
+                    points += 1;
+                }
+            }
+            if (color != null)
+            {
+                if ((color == "White") && (counter > 0) && (grid[counter - 1][1].ToString() == "Green"))
+                {
+                    points += 1;
+                }
+                else if ((color == "Green") && (drink == "Coffee"))
+                {
+                    points += 1;
+                }
+                else if ((color == "Yellow") && (cigar == "Dunhill"))
+                {
+                    points += 1;
+                }
+                else if ((color == "Blue") && (counter > 0) && (grid[counter - 1][2].ToString() == "Norwegian"))
+                {
+                    points += 1;
+                }
+            }
+            if (cigar != null)
+            {
+                if ((cigar == "Pall Mall") && (pet == "Birds"))
+                {
+                    points += 1;
+                }
+                else if ((cigar == "Blue Master") && (drink == "Beer"))
+                {
+                    points += 1;
+                }
+                else if ((cigar == "Blends") && (grid[counter - 1][5].ToString() == "Cats"))
+                {
+                    points += 1;
+                }
+                else if ((cigar == "Blends") && (counter > 0) && (grid[counter - 1][3].ToString() == "Water"))
+                {
+                    points += 1;
+                }
+            }
+            if (pet != null)
+            {
+                if ((pet == "Horses") && (counter > 0) && (grid[counter - 1][4].ToString() == "Dunhill"))
+                {
+                    points += 1;
+                }
+            }
+            if (drink != null)
+            {
+                if ((drink == "Milk") && (number == 3))
+                {
+                    points += 1;
+                }
+            }
+            return points;
+        }
+
+        public static int Failed(int fail, String [] rules, String [] failed, Object[][] grid, int counter, int number, string color, string nationality, string drink, string cigar, string pet)
+        {
+            if (nationality != null)
+            {
+                if ((nationality == "Brit") && (color != "Red"))
+                {
+                    failed[fail] = rules[0].ToString();
+                    fail += 1;
+                }
+                else if ((nationality == "Swede") && (pet != "Dogs"))
+                {
+                    failed[fail] = rules[1].ToString();
+                    fail += 1;
+                }
+                else if ((nationality == "Dane") && (drink != "Tea"))
+                {
+                    failed[fail] = rules[2].ToString();
+                    fail += 1;
+                }
+                else if ((nationality == "Norwegian") && (number != 1))
+                {
+                    failed[fail] = rules[8].ToString();
+                    fail += 1;
+                }
+                else if ((nationality == "German") && (cigar != "Prince"))
+                {
+                    failed[fail] = rules[12].ToString();
+                    fail += 1;
+                }
+            }
+            if (color != null)
+            {
+                if ((color == "White") && (counter > 0) && (grid[counter - 1][1].ToString() != "Green"))
+                {
+                    failed[fail] = rules[3].ToString();
+                    fail += 1;
+                }
+                else if ((color == "Green") && (drink != "Coffee"))
+                {
+                    failed[fail] = rules[4].ToString();
+                    fail += 1;
+                }
+                else if ((color == "Yellow") && (cigar != "Dunhill"))
+                {
+                    failed[fail] = rules[6].ToString();
+                    fail += 1;
+                }
+                else if ((color == "Blue") && (counter > 0) && (grid[counter - 1][2].ToString() != "Norwegian"))
+                {
+                    failed[fail] = rules[13].ToString(); ;
+                    fail += 1;
+                }
+            }
+            if (cigar != null)
+            {
+                if ((cigar == "Pall Mall") && (pet != "Birds"))
+                {
+                    failed[fail] = rules[5].ToString();
+                    fail += 1;
+                }
+                else if ((cigar == "Blue Master") && (drink != "Beer"))
+                {
+                    failed[fail] = rules[11].ToString();
+                    fail += 1;
+                }
+                else if ((cigar == "Blends") && (grid[counter - 1][5].ToString() != "Cats"))
+                {
+                    failed[fail] = rules[9].ToString();
+                    fail += 1;
+                }
+                else if ((cigar == "Blends") && (counter > 0) && (grid[counter - 1][3].ToString() != "Water"))
+                {
+                    failed[fail] = rules[14].ToString();
+                    fail += 1;
+                }
+            }
+            if (pet != null)
+            {
+                if ((pet == "Horses") && (counter > 0) && (grid[counter - 1][4].ToString() != "Dunhill"))
+                {
+                    failed[fail] = rules[10].ToString();
+                    fail += 1;
+                }
+            }
+            if (drink != null)
+            {
+                if ((drink == "Milk") && (number != 3))
+                {
+                    failed[fail] = rules[7].ToString();
+                    fail += 1;
+                }
+            }
+            return fail;
+        }
+
+        public static void PrintRules(string [] rules)
+        {
+            System.Console.Write("Rules of the riddle");
+            foreach (string rule in rules)
+            {
+                System.Console.Write("\n" + rule);
+            }
+            Console.WriteLine("\n\nUsing the rules above, please enter your choices for the houses");
+        }
+
+        public static string Performance(int result)
+        {
+            string print;
+            if (result == 0)
+            {
+                print = "Failed";
+            }
+            else if (result == 14)
+            {
+                print = "Passed";
+            }
+            else
+            {
+                print = "Fair";
+            }
+            return print;
+        }
+
+        public static void PrintPerformance(String [] failed, int fail, string print, int result)
+        {
+            Console.WriteLine("\n\nPoints scored: " + result);
+            Console.WriteLine("Result: " + print);
+            Console.WriteLine("\nFailed: " + fail);
+            if (fail > 0)
+            {
+                Console.WriteLine("\nYou failed the riddles below");
+                foreach (string failed_rule in failed)
+                {
+                    System.Console.Write("\n" + failed_rule);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,8 @@ namespace einsteins_Riddle
     {
         static void Main(string[] args)
         {
-            int points = 0;
             int result = 0;
-            string print = "";
+            string print;
             int counter = 0;
             string[] rules = new string[15]
              {
@@ -31,12 +30,7 @@ namespace einsteins_Riddle
             object[][] grid = new object[5][];
             string [] failed = new string[14];
             int fail = 0;
-            System.Console.Write("Rules of the riddle");
-            foreach (string rule in rules)
-            {
-                System.Console.Write("\n"+rule);
-            }
-            Console.WriteLine("\n\nUsing the rules above, please enter your choices for the houses");
+            Methods.PrintRules(rules);
             while (counter <= 4)
             {
                 int _number = counter + 1;
@@ -55,186 +49,12 @@ namespace einsteins_Riddle
                 House Obj_House = new House(_number);
                 object[] values = new object[6] { Obj_House.Number, Obj_Person.Color, Obj_Person.Nationality, Obj_Person.Drink, Obj_Person.Cigarette, Obj_Person.Pet };
                 grid[counter] = values;
-                int Verify()
-                {
-                    if (Obj_Person.Nationality != null)
-                    {
-                        if ((Obj_Person.Nationality == "Brit") && (Obj_Person.Color == "Red"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Brit") && (Obj_Person.Color != "Red"))
-                        {
-                            failed[fail] = rules[0].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Swede") && (Obj_Person.Pet == "Dogs"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Swede") && (Obj_Person.Pet != "Dogs"))
-                        {
-                            failed[fail] = rules[1].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Dane") && (Obj_Person.Drink == "Tea"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Dane") && (Obj_Person.Drink != "Tea"))
-                        {
-                            failed[fail] = rules[2].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Norwegian") && (Obj_House.Number == 1))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "Norwegian") && (Obj_House.Number != 1))
-                        {
-                            failed[fail] = rules[8].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "German") && (Obj_Person.Cigarette == "Prince"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Nationality == "German") && (Obj_Person.Cigarette != "Prince"))
-                        {
-                            failed[fail] = rules[12].ToString();
-                            fail += 1;
-                        }
-                    }
-                     if (Obj_Person.Color != null)
-                    {
-                        if ((Obj_Person.Color == "White") && (counter > 0) && (grid[counter - 1][1].ToString() == "Green"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Color == "White") && (counter > 0) && (grid[counter - 1][1].ToString() != "Green"))
-                        {
-                            failed[fail] = rules[3].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Color == "Green") && (Obj_Person.Drink == "Coffee"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Color == "Green") && (Obj_Person.Drink != "Coffee"))
-                        {
-                            failed[fail] = rules[4].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Color == "Yellow") && (Obj_Person.Cigarette == "Dunhill"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Color == "Yellow") && (Obj_Person.Cigarette != "Dunhill"))
-                        {
-                            failed[fail] = rules[6].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Color == "Blue") && (counter > 0) && (grid[counter - 1][2].ToString() == "Norwegian"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Color == "Blue") && (counter > 0) && (grid[counter - 1][2].ToString() != "Norwegian"))
-                        {
-                            failed[fail] = rules[13].ToString(); ;
-                            fail += 1;
-                        }
-                    }
-                    if (Obj_Person.Cigarette != null)
-                    {
-                        if ((Obj_Person.Cigarette == "Pall Mall") && (Obj_Person.Pet == "Birds"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Pall Mall") && (Obj_Person.Pet != "Birds"))
-                        {
-                            failed[fail] = rules[5].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Blue Master") && (Obj_Person.Drink == "Beer"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Blue Master") && (Obj_Person.Drink != "Beer"))
-                        {
-                            failed[fail] = rules[11].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Blends") && (grid[counter - 1][5].ToString() == "Cats"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Blends") && (grid[counter - 1][5].ToString() != "Cats"))
-                        {
-                            failed[fail] = rules[9].ToString();
-                            fail += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Blends") && (counter > 0) && (grid[counter - 1][3].ToString() == "Water"))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Cigarette == "Blends") && (counter > 0) && (grid[counter - 1][3].ToString() != "Water"))
-                        {
-                            failed[fail] = rules[14].ToString();
-                            fail += 1;
-                        }
-                    }
-                    if (Obj_Person.Pet != null)
-                    {
-                         if ((Obj_Person.Pet == "Horses") && (counter > 0) && (grid[counter - 1][4].ToString() == "Dunhill"))
-                        {
-                             points += 1;
-                        }
-                        else if ((Obj_Person.Pet == "Horses") && (counter > 0) && (grid[counter - 1][4].ToString() != "Dunhill"))
-                        {   
-                            failed[fail] = rules[10].ToString();
-                            fail += 1;
-                        }
-                    }
-                    if (Obj_Person.Drink != null)
-                    {
-                       if ((Obj_Person.Drink == "Milk") &&  (Obj_House.Number == 3))
-                        {
-                            points += 1;
-                        }
-                        else if ((Obj_Person.Drink == "Milk") && (Obj_House.Number != 3))
-                        {
-                            failed[fail] = rules[7].ToString();
-                            fail += 1;
-                        }
-                    }
-                        return points;
-                 }
-                   result = Verify();
-                   counter++;
-                 }
-                 if (result == 0)
-                 {
-                     print = "Failed";
-                 }
-                 else if(result == 14)
-                 {
-                     print = "Passed";
-                 }
-                 else
-                 {
-                     print = "Fair";
-                 }
-            Console.WriteLine("\n\nPoints scored: "+result);
-            Console.WriteLine("Result: "+print);
-
-            Console.WriteLine("\nFailed: "+fail);
-            if (fail > 0) {
-                Console.WriteLine("\nYou failed the riddles below");
-                foreach (string failed_rule in failed)
-                {
-                    System.Console.Write("\n" + failed_rule);
-                }
+                result = Methods.Points(result,counter,grid, Obj_House.Number, Obj_Person.Color, Obj_Person.Nationality, Obj_Person.Drink, Obj_Person.Cigarette, Obj_Person.Pet);
+                fail = Methods.Failed(fail,rules,failed,grid,counter, Obj_House.Number, Obj_Person.Color, Obj_Person.Nationality, Obj_Person.Drink, Obj_Person.Cigarette, Obj_Person.Pet);
+                counter++;
             }
+            print = Methods.Performance(result);
+            Methods.PrintPerformance(failed, fail, print,result);
         }
     }
 }
